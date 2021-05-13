b34e9a951b4b13aa3d374e045629de478a190191
Chelsea Roston <cmroston@gmail.com>
2021-05-13 19:31:18 -0400

    adds routes for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,45 @@ Rails.application.routes.draw do
     get "/users/:id" => "users#show"
 
     post "/sessions" => "sessions#create"
+
+    get "stories" => "stories#index"
+    get "stories" => "stories#show"
+    post "stories" =>
+    "stories#create"
+    patch "stories" => "stories#update"
+    delete "stories" => 
+    "stories#delete"
+
+    get "chapters" => "chapters#index"
+    get "chapters" => "chapters#show"
+    post "chapters" =>
+    "chapters#create"
+    patch "chapters" => "chapters#update"
+    delete "chapters" => 
+    "chapters#delete"
+
+    get "characters" => "characters#index"
+    get "characters" => "characters#show"
+    post "characters" =>
+    "characters#create"
+    patch "characters" => "characters#update"
+    delete "characters" => 
+    "characters#delete"
+
+    get "teams" => "teams#index"
+    get "teams" => "teams#show"
+    post "teams" =>
+    "teams#create"
+    patch "teams" => "teams#update"
+    delete "teams" => 
+    "teams#delete"
+
+    get "members" => "members#index"
+    get "members" => "members#show"
+    post "members" =>
+    "members#create"
+    patch "members" => "members#update"
+    delete "members" => 
+    "members#delete"
   end
 end

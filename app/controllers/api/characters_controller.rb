73984f758308883e550d730ede6d4ffbbbd533db
Chelsea Roston <cmroston@gmail.com>
2021-06-07 15:27:24 -0400

    adds show for characters

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -2,4 +2,9 @@ class Api::CharactersController < ApplicationController
   def index
     @characters = Character.where(story_id: params[:id])
   end
+
+  def show 
+    @characters = Character.find(params[:id])
+    render "show.json.jb"
+  end
 end

2c68099b7d70361b31515f3b29e59b700e9bc578
Chelsea Roston <cmroston@gmail.com>
2021-06-09 14:10:46 -0400

    updates team ccontroller

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -1,2 +1,23 @@
 class Api::TeamsController < ApplicationController
+  def index 
+    @teams = current_user.teams
+  end
+
+  def show
+    team_id = params[:id]
+    @team = Team.find(id: team_id)
+  end
+
+  def create 
+    @team = Team.new(
+      name: params[:id]
+    )
+    if @team.save
+      @user = current_user.id 
+      @team.members.create!(team_id: @team.id, user_id: @user)  
+    else 
+      render json: {errors: @team.errors.full_messages }, status: 406
+    end
+  end
+
 end
